ffe7494dea6040984d93eec1a877f06d509ec65e
Luann2003 <lvictorluciano@outlook.com>
2023-10-19 10:34:45 -0300

    First

---
+++ src/main/java/com/managementprojects/ManagementProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.managementprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/managementprojects/ManagementProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managementprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
